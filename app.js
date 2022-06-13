268eb6a7fc49b95d812bb17a2970cfd5f16f9aad
Snake <104637958+KirillChertov@users.noreply.github.com>
2022-06-13 20:23:36 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+$(function() {
+
+
+    /* Fixed Header */
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    checkScroll(scrollPos, introH)
+
+    $(window).on("scroll resize", function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if( scrollPos > introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+}
+
+
+
+    /*Smoot scroll*/
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        $("html, body").animate ({
+            scrollTop: elementOffset -57
+        }, 600);
+
+    });
+
+});
